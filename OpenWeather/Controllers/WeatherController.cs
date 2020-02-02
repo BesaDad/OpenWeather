56f85632242cb4178ffdfd18ba173d8cd2578626
beslan <metallik91@mail.ru>
2020-02-02 23:36:38 +0300

    initial code

---
+++ OpenWeather/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Infrastructure.Business;
+
+namespace OpenWeather.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        private readonly IWeatherService _weatherService;
+
+        public WeatherController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        public ActionResult GetWeatherByCityName()
+        {
+            return View();
+        }
+    }
+}
